e4c3478b2830cd297fd995bf13e42d91fb8cb76c
keynesisnotdead <70729951+MartinBasualdo0@users.noreply.github.com>
2023-10-11 18:47:48 -0300

    main

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ import src.brecha as bre
 import src.periodos_brecha as pbre
 import src.tcreal as tcr
 
-sbcra.main()
-print("Scrap BCRA terminado")
-bre.main()
-print("Brecha terminado")
-pbre.main()
-print("Períodos brecha terminado")
-tcr.main()
-print("Terminado tipo de cambio real")
-print("Programa terminado")
-    
+if __name__=="__main__":
+    sbcra.main()
+    print("Scrap BCRA terminado")
+    bre.main()
+    print("Brecha terminado")
+    pbre.main()
+    print("Períodos brecha terminado")
+    tcr.main()
+    print("Terminado tipo de cambio real")
+    print("Programa terminado")
